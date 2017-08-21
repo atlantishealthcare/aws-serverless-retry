7f18067757bc1248c0c03a57f52fb3b0dc092e3c
Pradeep Vattikuti <pradeep.vattikuti@atlantishealthcare.com>
2017-08-21 17:38:22 +1200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# aws-serverless-retry
-Checks error code
+# Simple library to retry your payload using SNS
+
+Use SNS service subscription functionality to hook your AWS services to create, publish and retry
+
+Actions you can perform:  
+- Create Topic
+- Publish to Topic by StatusCode 
+
+    Payload is published to appropriate topic based on statusCodes provided in configuration. If StatusCode is not listed in configuration it is
+    published to success topic by default.
+- Publish to Topic by Topic name
+
+Note: All functions return promise
+
+## Installing
+
+### In Node.js
+
+The preferred way to install the AWS Serverless Retry  for Node.js is to use the npm package manager for Node.js.
+Simply type the following into a terminal window:
+
+```sh
+npm install aws-serverless-retry
+```
+
+##Usage and Getting Started
+
+```javascript
+import ASR = require("aws-serverless-retry");
+
+//Create SNSNotifier and pass your configuration
+let config = {
+            region: "us-west-2",
+            retryStatusCodes: [500],
+            errorStatusCodes: [400],
+            successStatusCodes: [200, 201],
+            maxRetryAttempts: 2,
+            retryTopicName: "retry-topic",
+            successTopicName: "success-topic",
+            errorTopicName: "error-topic"
+        };
+let snsNotifier = new SNSNotifier(config);
+
+//Params
+let statusCode = 200;
+let payload = {
+  "data": "Test"
+};
+
+//Now call sendToTopicByStatusCode by passing your parameters above
+snsNotifier.sendToTopicByStatusCode(statusCode, payload)
+           .then(response => {
+                //Success     
+           })
+           .catch(err => {
+                //Error
+           });
+
+//Response
+{
+    topicName: "Success-Topic-Name",
+    .. //Stadard object that is returned from AWS-SDK
+}
+```
---
+++ tests/testSendToTopicByStatusCode.js
@@ -0,0 +1,152 @@
+"use strict";
+
+const SNSNotifier = require("../lib/sns_notifier");
+
+module.exports.sendToTopicByStatusCodeTests = {
+    testToSendItToSuccessTopicOn200: function (test) {
+        let config = {
+            region: "us-west-2",
+            retryStatusCodes: [],
+            errorStatusCodes: [],
+            successStatusCodes: [200],
+            maxRetryAttempts: 2,
+            retryTopicName: "retry-topic",
+            successTopicName: "success-topic",
+            errorTopicName: "error-topic"
+        };
+
+        let snsNotifier = new SNSNotifier(config);
+        let statusCode = 200;
+        let payload = {
+            "data": "Test"
+        };
+
+        snsNotifier.sendToTopicByStatusCode(statusCode, payload)
+            .then(response => {
+                test.ok(response !== null);
+                test.ok(response.topicName === config.successTopicName);
+                test.done();
+            });
+    },
+    testToSendItToErrorTopicOn400: function (test) {
+        let config = {
+            region: "us-west-2",
+            retryStatusCodes: [],
+            errorStatusCodes: [400],
+            successStatusCodes: [200],
+            maxRetryAttempts: 2,
+            retryTopicName: "retry-topic",
+            successTopicName: "success-topic",
+            errorTopicName: "error-topic"
+        };
+
+        let snsNotifier = new SNSNotifier(config);
+        let statusCode = 400;
+        let payload = {
+            "data": "Test"
+        };
+
+        snsNotifier.sendToTopicByStatusCode(statusCode, payload)
+            .then(response => {
+                test.ok(response !== null);
+                test.ok(response.topicName === config.errorTopicName);
+                test.done();
+            });
+    },
+    testToSendItToRetryTopicOn500: function (test) {
+        let config = {
+            region: "us-west-2",
+            retryStatusCodes: [500],
+            errorStatusCodes: [400],
+            successStatusCodes: [200],
+            maxRetryAttempts: 2,
+            retryTopicName: "retry-topic",
+            successTopicName: "success-topic",
+            errorTopicName: "error-topic"
+        };
+
+        let snsNotifier = new SNSNotifier(config);
+        let statusCode = 500;
+        let payload = {
+            "data": "Test"
+        };
+
+        snsNotifier.sendToTopicByStatusCode(statusCode, payload)
+            .then(response => {
+                test.ok(response !== null);
+                test.ok(response.topicName === config.retryTopicName);
+                test.done();
+            });
+    },
+    testToSendItToSuccessTopicOnIfUnknown: function (test) {
+        let config = {
+            region: "us-west-2",
+            retryStatusCodes: [500],
+            errorStatusCodes: [400],
+            successStatusCodes: [200],
+            maxRetryAttempts: 2,
+            retryTopicName: "retry-topic",
+            successTopicName: "success-topic",
+            errorTopicName: "error-topic"
+        };
+
+        let snsNotifier = new SNSNotifier(config);
+        let statusCode = 201;
+        let payload = {
+            "data": "Test"
+        };
+
+        snsNotifier.sendToTopicByStatusCode(statusCode, payload)
+            .then(response => {
+                test.ok(response !== null);
+                test.ok(response.topicName === config.successTopicName);
+                test.done();
+            });
+    },
+    testToFailIfStatusCodeIsInvalid: function (test) {
+        let config = {
+            region: "us-west-2",
+            retryStatusCodes: [500],
+            errorStatusCodes: [400],
+            successStatusCodes: [200],
+            maxRetryAttempts: 2,
+            retryTopicName: "retry-topic",
+            successTopicName: "success-topic",
+            errorTopicName: "error-topic"
+        };
+
+        let snsNotifier = new SNSNotifier(config);
+        let payload = {
+            "data": "Test"
+        };
+
+        snsNotifier.sendToTopicByStatusCode(null, payload)
+            .catch(err => {
+                test.ok(err !== null);
+                test.ok(err.message === "StatusCode is invalid");
+                test.done();
+            });
+    },
+    testToFailIfPayLoadIsInvalid: function (test) {
+        let config = {
+            region: "us-west-2",
+            retryStatusCodes: [500],
+            errorStatusCodes: [400],
+            successStatusCodes: [200],
+            maxRetryAttempts: 2,
+            retryTopicName: "retry-topic",
+            successTopicName: "success-topic",
+            errorTopicName: "error-topic"
+        };
+
+        let snsNotifier = new SNSNotifier(config);
+        let statusCode = 201;
+
+        snsNotifier.sendToTopicByStatusCode(statusCode)
+            .catch(err => {
+                test.ok(err !== null);
+                test.ok(err.message === "PayLoad is empty");
+                test.done();
+            });
+    }
+};
